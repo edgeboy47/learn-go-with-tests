50142a6d82f5fc175fde0cb4565821aa2cb282b6
Darrion Mills <dmills1994@gmail.com>
2024-04-09 22:01:32 -0400

    Add Triangle Shape, improve failed test output

--- structs/structs.go
+++ structs/structs.go
@@ -11,6 +11,11 @@ type Circle struct {
   radius float64
 }
 
+type Triangle struct {
+  base float64
+  height float64
+}
+
 type Shape interface {
   Area() float64
   Perimeter() float64
@@ -24,10 +29,18 @@ func (circle Circle) Perimeter() float64 {
   return 2 * math.Pi * circle.radius
 }
 
+func (triangle Triangle) Perimeter() float64 {
+  return 0.0
+}
+
 func (rectangle Rectangle) Area() float64 {
   return rectangle.width * rectangle.height
 }
 
 func (circle Circle) Area() float64 {
   return math.Pi * math.Pow(circle.radius, 2)
+}
+
+func (triangle Triangle) Area() float64 {
+  return triangle.base * triangle.height * 0.5
 }
--- structs/structs_test.go
+++ structs/structs_test.go
@@ -21,17 +21,33 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
+		name     string
 		shape    Shape
 		expected float64
 	}{
-		{Rectangle{5.0, 4.0}, 20.0},
-		{Circle{10.0}, 314.1592653589793},
+		{
+			name:     "Rectangle",
+			shape:    Rectangle{5.0, 4.0},
+			expected: 20.0,
+		},
+		{
+			name:     "Circle",
+			shape:    Circle{10.0},
+			expected: 314.1592653589793,
+		},
+		{
+			name:     "Triangle",
+			shape:    Triangle{10.0, 5.0},
+			expected: 25.0,
+		},
 	}
 
 	for _, shape := range areaTests {
-		actual := shape.shape.Area()
-		if actual != shape.expected {
-			t.Errorf("got %g want %g", actual, shape.expected)
-		}
+		t.Run(shape.name, func(t *testing.T) {
+			actual := shape.shape.Area()
+			if actual != shape.expected {
+				t.Errorf("%v got %g want %g", shape.shape, actual, shape.expected)
+			}
+		})
 	}
 }
