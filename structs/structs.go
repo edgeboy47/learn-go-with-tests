49651fb9329d4adb1f4028de6ad40afbe614da74
Darrion Mills <dmills1994@gmail.com>
2024-04-09 21:38:03 -0400

    Add interface for Shapes

--- structs/structs.go
+++ structs/structs.go
@@ -11,14 +11,22 @@ type Circle struct {
   radius float64
 }
 
-func Perimeter(rectangle Rectangle) float64 {
+type Shape interface {
+  Area() float64
+  Perimeter() float64
+}
+
+func (rectangle Rectangle) Perimeter() float64 {
   return (rectangle.width + rectangle.height) * 2.0
+}
+
+func (circle Circle) Perimeter() float64 {
+  return 2 * math.Pi * circle.radius
 }
 
 func (rectangle Rectangle) Area() float64 {
   return rectangle.width * rectangle.height
 }
-
 
 func (circle Circle) Area() float64 {
   return math.Pi * math.Pow(circle.radius, 2)
--- structs/structs_test.go
+++ structs/structs_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
-	actual := Perimeter(rectangle)
+	actual := rectangle.Perimeter()
 	expected := 40.0
 
 	if actual != expected {
@@ -13,25 +13,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
+  checkArea := func(t testing.TB, shape Shape, want float64) {
+    t.Helper()
+    got := shape.Area()
+    if got != want {
+      t.Errorf("got %g want %g", got, want)
+    }
+  }
+
 	t.Run("Rectangle", func(t *testing.T) {
 		rectangle := Rectangle{5.0, 4.0}
-
-		actual := rectangle.Area()
 		expected := 20.0
-
-		if actual != expected {
-			t.Errorf("got %.2f want %.2f", actual, expected)
-		}
+    checkArea(t, rectangle, expected)
 	})
 
 	t.Run("Circles", func(t *testing.T) {
 		circle := Circle{10.0}
-
-		actual := circle.Area()
 		expected := 314.1592653589793
-
-		if actual != expected {
-			t.Errorf("got %.2f want %.2f", actual, expected)
-		}
-	})
+    checkArea(t, circle, expected)
+  })
 }
